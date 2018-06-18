dd6f423bf2afa29ddb03ecfd8dd400f3a3e63cb7
venkatesh v <venkatesh_V@trimble.com>
2018-06-18 11:25:54 +0530

    changes in protofile plugin config

--- ConvertProtoFilesPlugin.js
+++ ConvertProtoFilesPlugin.js
@@ -7,12 +7,7 @@ class ConvertProtoFilesPlugin {
 		if (!this.isObject(options)) {
 			throw new Error('options should be object');
 		}
-		this.destFolder = options.destFolder;
-		this.protoFileName = options.protoFileName;
-		this.toAppend = options.toAppend;
-		this.toPrepend = options.toPrepend;
-		this.jsFileName = options.jsFileName;
-		this.tsFileName = options.tsFileName
+		this.options = options;
 	}
 
 	isObject(obj) {
@@ -20,30 +15,31 @@ class ConvertProtoFilesPlugin {
 	}
 
 	apply(compiler) {
+		
 		compiler.hooks.afterEmit.tap('convert', (compilation, callback) => {
-			if (!this.destFolder || !this.protoFileName) {
+			if (!this.options.destFolder || !this.options.protoFileName) {
 				throw new Error('destination Folder or Output File name not defined in options'); ;
 			}
-			if (fs.existsSync(this.destFolder + this.protoFileName)) {
-				fs.unlink(this.destFolder + this.protoFileName, () => {
+			if (fs.existsSync(this.options.destFolder + this.options.protoFileName)) {
+				fs.unlink(this.options.destFolder + this.options.protoFileName, () => {
 				});
 			}
-			fs.readdir(this.destFolder, (err, data) => {
+			fs.readdir(this.options.destFolder, (err, data) => {
 				if (err) return err;
 				data.forEach(file => {
-					let fileImport = this.toAppend + file + this.toPrepend;
-					fs.appendFile(this.destFolder + this.protoFileName, fileImport, err => {
+					let fileImport = this.options.toAppend + file + this.options.toPrepend;
+					fs.appendFile(this.options.destFolder + this.options.protoFileName, fileImport, err => {
 						if (err) return err
 					});
 				});
 
 				var pbjs_cmd = path.resolve('node_modules/protobufjs/bin/pbjs');
-				var js = spawn('node', [pbjs_cmd, path.normalize(this.destFolder + 'proto-yams.proto'), '--keep-case', '--force-message', '-t', "static-module", '--no-verify', '--no-convert', '--no-delimited', '-o', path.normalize(this.destFolder + this.jsFileName)]);
-				js.stderr.on('data', (data) => { console.log(data.toString()) })
+				var js = spawn('node', [pbjs_cmd, path.normalize(this.options.destFolder + 'proto-yams.proto'), '--keep-case', '--force-message', '-t', "static-module", '--no-verify', '--no-convert', '--no-delimited', '-o', path.normalize(this.options.destFolder + this.options.jsFileName)]);
+				js.stderr.on('data', (data) => { return data.toString() });
 
 				var pbts_cmd = path.resolve('node_modules/protobufjs/bin/pbts');
-				var ts = exec('node ' + pbjs_cmd + ' -t static-module ' + path.normalize(this.destFolder + 'proto-yams.proto') + ' --keep-case --force-message --no-verify --no-convert --no-delimited | node ' + pbts_cmd + ' -o ' + path.normalize(this.destFolder + this.tsFileName) + ' -');
-				ts.stderr.on('data', (data) => { console.log(data.toString()) })
+				var ts = exec('node ' + pbjs_cmd + ' -t static-module ' + path.normalize(this.options.destFolder + 'proto-yams.proto') + ' --keep-case --force-message --no-verify --no-convert --no-delimited | node ' + pbts_cmd + ' -o ' + path.normalize(this.options.destFolder + this.options.tsFileName) + ' -');
+				ts.stderr.on('data', (data) => { return data.toString() });
 
 			});
 		});
